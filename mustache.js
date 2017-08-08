1e7d3ce673056703b38598c3a70bb5646dbdd4cf
Adam Hammond <adam.j.hammond@gmail.com>
2017-08-08 13:18:25 +0100

    First version

--- mustache.js
+++ mustache.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 IBM Corp.
+ * Copyright JS Foundation and other contributors, http://js.foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,11 +18,53 @@ module.exports = function(RED) {
     "use strict";
     var mustache = require("mustache");
 
+    /**
+     * Custom Mustache Context capable to resolve message property and node
+     * flow and global context
+     */
+    function NodeContext(msg, nodeContext,parent) {
+        this.msgContext = new mustache.Context(msg,parent);
+        this.nodeContext = nodeContext;
+    }
+
+    NodeContext.prototype = new mustache.Context();
+
+    NodeContext.prototype.lookup = function (name) {
+        // try message first:
+        try {
+            var value = this.msgContext.lookup(name);
+            if (value !== undefined) {
+                return value;
+            }
+
+            // try node context:
+            var dot = name.indexOf(".");
+            if (dot > 0) {
+                var contextName = name.substr(0, dot);
+                var variableName = name.substr(dot + 1);
+
+                if (contextName === "flow" && this.nodeContext.flow) {
+                    return this.nodeContext.flow.get(variableName);
+                }
+                else if (contextName === "global" && this.nodeContext.global) {
+                    return this.nodeContext.global.get(variableName);
+                }
+            }
+        } catch(err) {
+            throw err;
+        }
+    }
+
+    NodeContext.prototype.push = function push (view) {
+            return new NodeContext(view, this.nodeContext,this.msgContext);
+    };
+
     function MustacheNode(n) {
         RED.nodes.createNode(this,n);
         this.name = n.name;
         this.field = n.field || "payload";
         this.fieldType = n.fieldType || "msg";
+        this.outputFormat = n.output || "str";
 
         var node = this;
         node.on("input", function(msg) {
@@ -31,7 +73,12 @@ module.exports = function(RED) {
                     throw new Error("No msg.template specified. Nothing to render");
                 }
 
-                var value = mustache.render(msg.template,msg.payload);
+                var value = mustache.render(msg.template,new NodeContext(msg, node.context()));
+
+                if (node.outputFormat === "json") {
+                    value = JSON.parse(value);
+                }
+
                 if (node.fieldType === 'msg') {
                     RED.util.setMessageProperty(msg,node.field,value);
                 } else if (node.fieldType === 'flow') {
